83c4bfdc2f08f7086e9fc9f518a52c15cd9a1f7e
victor_rodriguez <victor.rodriguez.zafra@gmail.com>
2026-04-30 12:59:34 +0200

    ci(terraform): añade despliegue automático con LocalStack

--- main.tf
+++ main.tf
@@ -8,14 +8,19 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
   endpoints {
     s3 = "http://localhost:4566"
   }
 }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "mi-bucket-test"
+resource "aws_s3_bucket" "bucket_prueba" {
+  bucket = "bucket-localstack-tarea6"
 }
